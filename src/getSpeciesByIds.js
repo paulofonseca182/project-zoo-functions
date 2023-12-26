2e7effcd1626c8a64e3f9604e3135adaf44ee7a9
PauloFonseca <pfonseca.arq@gmail.com>
2023-12-26 13:59:03 -0300

    Implemente a função getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código
+function getSpeciesByIds(...ids) {
+  const animalArray = [];
+  ids.forEach((e) => animalArray.push(...data.species.filter((specie) => specie.id === e)));
+  return animalArray;
 }
 
 module.exports = getSpeciesByIds;
