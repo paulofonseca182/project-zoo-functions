b9e2859bccd1f38f3f26b37bca8dc70fa6f45ab3
PauloFonseca <pfonseca.arq@gmail.com>
2023-12-26 17:55:52 -0300

    Crie a gestão das pessoas colaboradoras de acordo com o respectivo gerente

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const { employees } = data;
+  const verifyManagers = employees.filter(({ managers }) => managers.includes(id));
+
+  if (verifyManagers.length !== 0) {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const { employees } = data;
+    const verifyManagers = employees.filter(({ managers }) => managers.includes(managerId));
+
+    return verifyManagers.map((e) => `${e.firstName} ${e.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
