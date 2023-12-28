c228c11d128013992a798085534a3569250f64c5
PauloFonseca <pfonseca.arq@gmail.com>
2023-12-28 15:41:52 -0300

    Implemente a função  que calcula o valor total da entrada

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,50 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const totalEntrants = {};
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      child += 1;
+    } if (entrant.age >= 18 && entrant.age < 50) {
+      adult += 1;
+    } if (entrant.age >= 50) {
+      senior += 1;
+    }
+  });
+  totalEntrants.child = child;
+  totalEntrants.adult = adult;
+  totalEntrants.senior = senior;
+  return totalEntrants;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let totalValue = 0;
+
+  if (!entrants || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const totalEntrants = countEntrants(entrants);
+
+  totalValue += totalEntrants.child * prices.child;
+  totalValue += totalEntrants.adult * prices.adult;
+  totalValue += totalEntrants.senior * prices.senior;
+  return totalValue;
 }
+
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
+console.log(calculateEntry(entrants));
 
 module.exports = { calculateEntry, countEntrants };
