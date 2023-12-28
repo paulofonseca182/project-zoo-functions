dd56f0a378d9e7a5cb93e38cf8e2607890cc903a
PauloFonseca <pfonseca.arq@gmail.com>
2023-12-28 10:50:29 -0300

    Implemente a função  que contabiliza a quantidade de espécies de animais residentes no zoológico

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const { species } = data;
+  if (!animal) {
+    const allAnimals = species.reduce((acc, arr) => {
+      acc[arr.name] = arr.residents.length;
+      return acc;
+    }, {});
+    return allAnimals;
+  }
+  const { specie, sex } = animal;
+  if (!sex) {
+    const specieAnimal = species.find((e) => e.name === specie).residents.length;
+    return specieAnimal;
+  }
+  const specieAnimalSex = species.find((e) => e.name === specie)
+    .residents.filter((e) => e.sex === sex).length;
+  return specieAnimalSex;
 }
+
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
