02a4a9a0003d03bf9da37dcc58dc6596cf076518
PauloFonseca <pfonseca.arq@gmail.com>
2023-12-29 14:34:26 -0300

    Implemente a função  para encontrar o animal mais velho da espécie gerenciado por uma pessoa colaboradora

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getEmployee = employees.filter((employee) => employee.id === id)[0].responsibleFor[0];
+
+  const getSpecie = species.filter((specie) => specie.id === getEmployee)[0].residents;
+  let hightAge = {
+    name: '',
+    sex: '',
+    age: 0,
+  };
+  getSpecie.forEach((e) => {
+    if (e.age > hightAge.age) {
+      hightAge = e;
+    }
+  });
+
+  return Object.values(hightAge);
 }
 
 module.exports = getOldestFromFirstSpecies;
