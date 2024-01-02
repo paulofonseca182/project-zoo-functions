cdeac9c83731e8c581a89846368fa1ac630dba94
PauloFonseca <pfonseca.arq@gmail.com>
2024-01-02 14:16:05 -0300

    Implemente a função  para disponibilizar um cronograma com os horários de visita da semana disponíveis para cada espécie de animal

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,6 +1,33 @@
 const data = require('../data/zoo_data');
 
+const { hours, species } = data;
+const getDays = Object.keys(hours);
+const allDays = getDays.reduce((acc, arr) => {
+  acc[arr] = {
+    officeHour: `Open from ${hours[arr].open}am until ${hours[arr].close}pm`,
+    exhibition: species.filter((e) => e.availability
+      .includes(arr)).map((specie) => specie.name) };
+  return acc;
+}, {});
+
+allDays.Monday.exhibition = 'The zoo will be closed!';
+allDays.Monday.officeHour = 'CLOSED';
+
 function getSchedule(scheduleTarget) {
+  const getSpecie = species.filter((specie) => specie.name === scheduleTarget);
+
+  if (getDays.includes(scheduleTarget)) {
+    const day = {};
+    day[scheduleTarget] = allDays[scheduleTarget];
+    return day;
+  }
+  if (scheduleTarget === undefined || getSpecie.length === 0) {
+    return allDays;
+  }
+  if (getSpecie) {
+    return getSpecie[0].availability;
+  }
 }
 
+console.log(getSchedule('asdas'));
 module.exports = getSchedule;
